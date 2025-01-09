0bf1d9b14588b0f3651831a65f4b71c34e10d793
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-01-09 13:31:08 +0100

    add setting view and toolbar

---
+++ TrackUp/Core/Settings/SettingsView.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsView.swift
+//  TrackUp
+//
+//  Created by Nicolas Borlet on 09/01/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            Text("Settings View")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
--- TrackUp/Core/Statistics/StatisticsView.swift
+++ TrackUp/Core/Statistics/StatisticsView.swift
@@ -1,11 +1,43 @@
 import SwiftUI
 
 struct StatisticsView: View {
+    @State private var index: Int = 0
+    @State private var showSettings = false
+    
     var body: some View {
-        VStack {
-            Text("Statistiques")
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    
+                    Text("Statistiques")
+                    
+                    Picker("", selection: $index){
+                        Text("Weekly").tag(0)
+                        Text("Monthly").tag(1)
+                        Text("Yearly").tag(2)
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    Text("Selected: \(index)")
+                }
+                .padding()
+            }
+            .navigationTitle("Spending")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        showSettings.toggle()
+                    } label: {
+                        Image(systemName: "gear")
+                    }
+                }
+            }
+            .sheet(isPresented: $showSettings) {
+                SettingsView()
+            }
+            // Make the toolbar background white with blur
+            .toolbarBackground(.white, for: .navigationBar)
+            .withGradientBackground()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .withGradientBackground()
     }
 }
